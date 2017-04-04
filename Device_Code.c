9e8f9717f724eea2453df4a07251bbb4169c9d78
cosmological <emidyp@mssm.org>
2017-04-04 18:01:44 -0400

    Added c files
    Added basic c files for pseudocode

---
+++ Device_Code.c
@@ -0,0 +1,16 @@
+/*Peripheral Thermostat System
+Auxillary Device
+*Collects temperature data and sends it to HUB
+*Interprets data from internal and external temperature sensors
+**Puts data into packet
+**Sends packet
+
+Group 7
+Joseph Asante
+Peter Emidy
+Abdul Hassan
+*/
+
+int main() {
+	//Do Code Stuff here
+}
---
+++ HUB_Code.c
@@ -0,0 +1,17 @@
+/*Peripheral Thermostat System
+Smart Hub - The brains of the system 
+*Collects Data from all auxilary sensors and interprets itoa
+*Allows for user interaction
+**Set sensor target temperatures
+**Name sensors
+**View sensor information
+
+Group 7
+Joseph Asante
+Peter Emidy
+Abdul Hassan
+*/
+
+int main() {
+	//Do code stuff here
+}
