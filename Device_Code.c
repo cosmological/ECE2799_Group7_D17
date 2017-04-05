9e3a74f30a5753f5dac5c1ed134386bc554b560e
jbasante <jbasante@wpi.edu>
2017-04-04 20:24:07 -0400

    Some Functions

--- Device_Code.c
+++ Device_Code.c
@@ -14,3 +14,25 @@ Abdul Hassan
 int main() {
 	//Do Code Stuff here
 }
+
+/*Function: This function  
+
+void buttonPress(){
+}
+
+/*Function: This function connects or disconnects the device to and from the hub. It will be called whenever 
+the connect button is pressed*/  
+void connectionToHub(){
+}
+
+/*Function: This function triggers the temperature sensor to measure the temperature of its surrounding
+every five minutes
+Reuturns: An integer which is the recorded value of the temperature*/
+int acquireTemp(){
+}
+
+/*Function: This function receives information (data) from the temperature sensor and sends 
+it to the hub. Function will be called everytime after temperature is measured by the sensor (every 5 minutes)*/
+void sendPacket(packet){
+}
+
